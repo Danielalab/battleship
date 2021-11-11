dc9a35b00dd4c5e84d418d87f00f7d0cc0644bb1
Danielalab <olabolacrayola@hotmail.com>
2021-11-10 23:11:05 -0500

    feat(controllers): add fn to build initial table with random ships

---
+++ src/controllers/buildShips.js
@@ -0,0 +1,57 @@
+const ships = [
+  { type: 'battleship', size: 4, total: 1 },
+  { type: 'submarine', size: 3, total: 2 },
+  { type: 'cruiser', size: 2, total: 3 },
+  { type: 'destroyer', size: 1, total: 4 },
+];
+
+const getShipPositions = (ship, initialPosition, offset) => {
+  const positions = [];
+  for (let index = 0; index < ship.size; index += 1) {
+    positions.push(initialPosition + (index * offset));
+  }
+  return positions;
+};
+
+const buildShip = (squares, ship) => {
+  /* direction:
+  - 0 => 'horizontal'
+  - 1 => 'vertical' */
+  const direction = Math.floor(Math.random() * 2);
+  const offset = (direction ? 10 : 1); // squares between each position
+  const randomPosition = Math.abs(
+    Math.floor(Math.random() * 100 - (ship.size * offset)),
+  );
+  const shipPositions = getShipPositions(ship, randomPosition, offset);
+  const positionIsFilled = shipPositions.some((position) => squares[position].isFilled);
+  const shipIsAtRightBorder = shipPositions.some((position) => position % 10 === 9);
+  const shipIsAtLeftBorder = shipPositions.some((position) => position % 10 === 0);
+
+  if (ship.size === 1 && !positionIsFilled) {
+    return shipPositions;
+  }
+  if (!positionIsFilled && !shipIsAtRightBorder && !shipIsAtLeftBorder) {
+    return shipPositions;
+  }
+  return buildShip(squares, ship);
+};
+
+const buildInitialTable = () => {
+  const squares = Array(100).fill({ isFilled: false });
+  ships.forEach((ship) => {
+    const { total, type } = ship;
+    for (let index = 0; index < total; index += 1) {
+      const shipSquares = buildShip(squares, ship);
+      shipSquares.forEach((squarePosition) => {
+        squares[squarePosition] = {
+          isFilled: true,
+          type,
+          shipSquares,
+        };
+      });
+    }
+  });
+  return squares;
+};
+
+export default buildInitialTable;
