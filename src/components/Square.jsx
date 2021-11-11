6b0a40c48384b8a499f5b0b80c3f1711be103153
Danielalab <olabolacrayola@hotmail.com>
2021-11-11 01:25:43 -0500

    style(Square): add icon & hover effect

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,20 +1,39 @@
 import styled from 'styled-components';
+import targetIcon from '../img/target.png';
 
-const shipColor = {
+/* const shipColor = {
   battleship: 'red',
   cruiser: 'pink',
   destroyer: 'green',
   submarine: 'orange',
-};
+}; */
 
-const chooseShipColor = ({ data }) => (data.isFilled ? shipColor[data.type] : '#1597E5');
+// const chooseShipColor = ({ data }) => (data.isFilled ? shipColor[data.type] : '#1597E5');
 
-const Square = styled.div`
-  background-color: ${chooseShipColor};
-  border: 1px solid #113CFC;
+const SquareStyled = styled.div`
+  background-color: #2E4B84;
+  border: 1px solid #1151B5;
   box-sizing: border-box;
+  cursor: pointer;
   height: 40px;
+  margin: 1px;
   width: 40px;
+  :hover > img {
+    background-color: #007a00;
+    visibility: visible;
+  }
 `;
+
+const Icon = styled.img`
+  padding: 4px;
+  visibility: hidden;
+  transition: all 0.05s ease-in;
+`;
+
+const Square = () => (
+  <SquareStyled>
+    <Icon src={targetIcon} alt="target icon" />
+  </SquareStyled>
+);
 
 export default Square;
