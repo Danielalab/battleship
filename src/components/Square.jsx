a0bd75c56fc6aa29c1c439eaee1d56906343c4a2
Danielalab <olabolacrayola@hotmail.com>
2021-11-12 00:14:39 -0500

    feat(Square): change color when shot misses

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,17 +1,19 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 import styled from 'styled-components';
 import targetIcon from '../img/target.png';
 
-/* const shipColor = {
+const squaresColor = {
   battleship: 'red',
   cruiser: 'pink',
   destroyer: 'green',
   submarine: 'orange',
-}; */
-
-// const chooseShipColor = ({ data }) => (data.isFilled ? shipColor[data.type] : '#1597E5');
+  ocean: '#1597E5',
+  default: '#2E4B84',
+};
 
 const SquareStyled = styled.div`
-  background-color: #2E4B84;
+  background-color: ${(props) => squaresColor[props.backgroundColor]};
   border: 1px solid #1151B5;
   box-sizing: border-box;
   cursor: pointer;
@@ -30,10 +32,30 @@ const Icon = styled.img`
   transition: all 0.05s ease-in;
 `;
 
-const Square = () => (
-  <SquareStyled>
-    <Icon src={targetIcon} alt="target icon" />
-  </SquareStyled>
-);
+const Square = ({ data }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [squareColor, setSquareColor] = useState('default');
+  const handleClick = () => {
+    const { isFilled } = data;
+    if (!isFilled) {
+      setSquareColor('ocean');
+    }
+    setIsClicked(true);
+  };
+
+  return (
+    <SquareStyled onClick={!isClicked ? handleClick : null} backgroundColor={squareColor}>
+      <Icon src={targetIcon} alt="target icon" />
+    </SquareStyled>
+  );
+};
 
 export default Square;
+
+Square.propTypes = {
+  data: PropTypes.shape({
+    isFilled: PropTypes.bool.isRequired,
+    type: PropTypes.string,
+    shipSquares: PropTypes.arrayOf(PropTypes.number),
+  }).isRequired,
+};
