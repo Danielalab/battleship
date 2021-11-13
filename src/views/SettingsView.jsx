aaf3080c08e4da7a8bdced478b0c86056b931e1b
Danielalab <olabolacrayola@hotmail.com>
2021-11-13 15:17:25 -0500

    feat(SettingsView): add styles to each component

--- src/views/SettingsView.jsx
+++ src/views/SettingsView.jsx
@@ -5,16 +5,42 @@ import styled from 'styled-components';
 import { levels } from '../util';
 
 const Label = styled.label`
+  color: #05FC32;
   font-size: 2rem;
+  margin: 0 0.75rem;
 `;
 
 const Input = styled.input`
+  background: transparent;
+  color: ${(props) => (props.readOnly ? '#ffffffa2' : '#ffffff')};
+  font-size: 1.5rem;
+  font-family: 'Press Start 2P', cursive;
+  margin: 0 0.75rem;
+  padding: 0.5rem 1rem;
   outline: 0;
 `;
 
 const Button = styled.button`
   cursor: pointer;
+  font-size: 1.5rem;
+  font-family: 'Press Start 2P', cursive;
+  margin: 0.75rem;
+  padding: 0.5rem 1rem;
   text-transform: uppercase;
+`;
+
+const ButtonSuccess = styled(Button)`
+  background-color: #113CFC;
+  color: #ffffff;
+  :disabled {
+    background-color: transparent;
+    color: #ffffffa2;
+  }
+`;
+
+const ButtonLevel = styled(Button)`
+  background-color: ${(props) => (props.selected ? '#FCC82B' : 'transparent')};
+  color: ${(props) => (props.selected ? '#000000' : '#ffffffa2')};
 `;
 
 const Field = styled.div`
@@ -47,26 +73,27 @@ const SettingsView = ({ levelSelected, saveLevel }) => {
       </Field>
       <Field>
         {levels.map((level) => (
-          <Button
+          <ButtonLevel
             key={level.id}
             selected={levelSelected === level.id}
             onClick={() => saveLevel(level.id)}
           >
             {level.name}
-          </Button>
+          </ButtonLevel>
         ))}
       </Field>
       <Field>
-        <Label htmlFor="user-chances">Choose my game chances</Label>
-        <Input type="number" id="user-chances" placeholder="20" min="20" />
+        <Label htmlFor="user-chances">
+          <Input type="number" id="user-chances" placeholder="20" min="20" readOnly={levelSelected !== 'custom'} />
+        </Label>
       </Field>
-      <Button
+      <ButtonSuccess
         type="submit"
         disabled={!nickname}
         onClick={handleSubmit}
       >
         start game
-      </Button>
+      </ButtonSuccess>
     </>
   );
 };
