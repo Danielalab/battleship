de146004c4c93e5be138c717a21f5cd403120bfc
Danielalab <olabolacrayola@hotmail.com>
2021-11-13 01:42:26 -0500

    feat(App): add settings route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import BattleshipView from './views/BattleshipView';
+import SettingsView from './views/SettingsView';
 
 function App() {
   const [gameLevel] = useState('easy');
   return (
     <Routes>
-      <Route path="/" element={<BattleshipView level={gameLevel} />} />
+      <Route path="/" element={<BattleshipView level={gameLevel} exact />} />
+      <Route path="/settings" element={<SettingsView />} exact />
     </Routes>
   );
 }
