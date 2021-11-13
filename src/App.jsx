9797ebfff9994140c271446f31bc06c27f3b868b
Danielalab <olabolacrayola@hotmail.com>
2021-11-13 13:54:09 -0500

    refactor(App): add event to save level selected

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import BattleshipView from './views/BattleshipView';
 import SettingsView from './views/SettingsView';
+import { levels } from './util';
 
 function App() {
-  const [gameLevel] = useState('easy');
+  const [gameLevel, setGameLevel] = useState('easy');
+  const [customChances, setCustomChances] = useState(undefined);
+  const chancesByLevel = levels.find((level) => level.id === gameLevel).chances;
   return (
     <Routes>
-      <Route path="/" element={<BattleshipView level={gameLevel} exact />} />
-      <Route path="/settings" element={<SettingsView />} exact />
+      <Route path="/" element={<BattleshipView initialChances={customChances || chancesByLevel} exact />} />
+      <Route
+        path="/settings"
+        element={(
+          <SettingsView
+            levelSelected={gameLevel}
+            saveLevel={setGameLevel}
+            saveCustomChances={setCustomChances}
+          />
+        )}
+        exact
+      />
     </Routes>
   );
 }
