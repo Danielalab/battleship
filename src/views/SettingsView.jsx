c345bc46400e43754a1af48704ecda874f85c18b
Danielalab <olabolacrayola@hotmail.com>
2021-11-13 02:01:43 -0500

    feat(SettingView): add event to nickname input

--- src/views/SettingsView.jsx
+++ src/views/SettingsView.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Label = styled.label`
@@ -17,25 +18,37 @@ const Field = styled.div`
   margin: 2rem 0;
 `;
 
-const SettingsView = () => (
-  <>
-    <Field>
-      <Label htmlFor="user-nickname">Nickname</Label>
-      <Input type="text" id="user-nickname" placeholder="Jack Sparrow" />
-    </Field>
-    <Field>
-      <Button>easy</Button>
-      <Button>medium</Button>
-      <Button>hard</Button>
-    </Field>
-    <Field>
-      <Label htmlFor="user-chances">Choose my game chances</Label>
-      <Input type="number" id="user-chances" placeholder="20" min="20" />
-    </Field>
-    <Button type="submit">
-      start game
-    </Button>
-  </>
-);
+const SettingsView = () => {
+  const [nickname, setNickname] = useState('');
+  const handleInput = (event) => {
+    const value = event.target.value.trim();
+    setNickname(value);
+  };
+  return (
+    <>
+      <Field>
+        <Label htmlFor="user-nickname">Nickname</Label>
+        <Input
+          type="text"
+          id="user-nickname"
+          placeholder="Jack Sparrow"
+          onInput={handleInput}
+        />
+      </Field>
+      <Field>
+        <Button>easy</Button>
+        <Button>medium</Button>
+        <Button>hard</Button>
+      </Field>
+      <Field>
+        <Label htmlFor="user-chances">Choose my game chances</Label>
+        <Input type="number" id="user-chances" placeholder="20" min="20" />
+      </Field>
+      <Button type="submit" disabled={!nickname}>
+        start game
+      </Button>
+    </>
+  );
+};
 
 export default SettingsView;
