cd27464b654ecd8ae83b9da305213b957df3789e
Danielalab <olabolacrayola@hotmail.com>
2021-11-13 15:16:20 -0500

    refactor(BattleshipView): move Container component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import styled from 'styled-components';
 import BattleshipView from './views/BattleshipView';
 import SettingsView from './views/SettingsView';
 import { levels } from './util';
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+`;
 
 function App() {
   const [gameLevel, setGameLevel] = useState('easy');
   const [customChances, setCustomChances] = useState(undefined);
   const chancesByLevel = levels.find((level) => level.id === gameLevel).chances;
   return (
-    <Routes>
-      <Route path="/" element={<BattleshipView initialChances={customChances || chancesByLevel} exact />} />
-      <Route
-        path="/settings"
-        element={(
-          <SettingsView
-            levelSelected={gameLevel}
-            saveLevel={setGameLevel}
-            saveCustomChances={setCustomChances}
-          />
+    <Container>
+      <Routes>
+        <Route path="/" element={<BattleshipView initialChances={customChances || chancesByLevel} exact />} />
+        <Route
+          path="/settings"
+          element={(
+            <SettingsView
+              levelSelected={gameLevel}
+              saveLevel={setGameLevel}
+              saveCustomChances={setCustomChances}
+            />
         )}
-        exact
-      />
-    </Routes>
+          exact
+        />
+      </Routes>
+    </Container>
   );
 }
 
