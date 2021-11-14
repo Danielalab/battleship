4fb26f0bffe9366c47fd5de741d0ff5cbcc3aeb3
Danielalab <olabolacrayola@hotmail.com>
2021-11-14 02:17:38 -0500

    refactor(Square): delete SkullIcon & add TargetIcon

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,38 +1,19 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import SquareStyled from './common/Square.styled';
+import Icon from './common/Icon.styled';
 import targetIcon from '../img/target.png';
 import equixIcon from '../img/equix.png';
-import { squaresColor, skullsIconsByShip } from '../util';
+import { skullsIconsByShip } from '../util';
 
-const SquareStyled = styled.div`
-  background-color: ${(props) => squaresColor[props.backgroundColor]};
-  border: 1px solid #1151B5;
-  box-sizing: border-box;
-  cursor: pointer;
-  height: 40px;
-  margin: 1px;
-  width: 40px;
-  transition: background-color 0.05s ease-in;
-  :hover > .target-icon {
-    background-color: #007a00;
-    visibility: visible;
-  }
-`;
-
-const Icon = styled.img`
-  padding: 4px;
+const TargetIcon = styled(Icon)`
   visibility: hidden;
-  transition: all 0.05s ease-in;
 `;
 
 const SuccessfulShotIcon = styled(Icon)`
   visibility: visible;
   padding: 8px;
-`;
-
-const SkullIcon = styled(Icon)`
-  visibility: visible;
 `;
 
 const Square = ({ data, subtractAChance, handleSuccesfulShot }) => {
@@ -63,8 +44,8 @@ const Square = ({ data, subtractAChance, handleSuccesfulShot }) => {
       onClick={!isClicked ? handleClick : null}
       backgroundColor={squareColor}
     >
-      {squareColor !== 'shot' && !isDestroyed && <Icon src={targetIcon} alt="target icon" className="target-icon" />}
-      {squareColor !== 'shot' && isDestroyed && <SkullIcon src={skullsIconsByShip[type]} />}
+      {squareColor !== 'shot' && !isDestroyed && <TargetIcon src={targetIcon} alt="target icon" className="target-icon" />}
+      {squareColor !== 'shot' && isDestroyed && <Icon src={skullsIconsByShip[type]} />}
       {squareColor === 'shot' && !isDestroyed && <SuccessfulShotIcon src={equixIcon} alt="Equix icon" />}
     </SquareStyled>
   );
