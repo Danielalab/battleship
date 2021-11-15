03d661647659fa13e908fe73a30d93344d0ad5b3
Danielalab <olabolacrayola@hotmail.com>
2021-11-14 22:29:17 -0500

    test(src): update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import 'jest-styled-components';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Buttons', () => {
+  test('Should renders Button component', () => {
+    /* render component with router */
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router location={history}>
+        <App />
+      </Router>,
+    );
+    expect(container).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
 });
